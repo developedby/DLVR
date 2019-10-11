ea62625fd6f1d1387a89fb84b4975419ceb46941
Nicolas Abril <nicolasabril@alunos.utfpr.edu.br>
2019-10-11 18:07:10 -0300

    Tira constantes sobreando

--- vehicle/include/vision.hpp
+++ vehicle/include/vision.hpp
@@ -9,10 +9,6 @@
 class Vision
 {
 	private:
-		float static const horizon_y = 0.282;
-		float static const real_zero_deg = 4.647;
-		float static const img_height = 960;
-		float static const img_width = 1280;
 		raspicam::RaspiCam_Cv cam;
 		cv::Mat img;
 		void getColorMask(cv::Mat& dst, int const h_min, int const h_max, int const l_min, int const l_max, int const s_min, int const s_max);
