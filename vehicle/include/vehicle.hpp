dfa540ddfe59c21aa444cfe75e06d4e035dcc9a5
Nicolas Abril <nicolasabril@alunos.utfpr.edu.br>
2019-10-12 16:54:34 -0300

    vehicle compiling

--- vehicle/include/vehicle.hpp
+++ vehicle/include/vehicle.hpp
@@ -1,7 +1,7 @@
 #ifndef vehicle_hpp_
 #define vehicle_hpp_
 
-//#include "vision.hpp"
+#include "vision.hpp"
 #include "movement.hpp"
 #include "radio_communication.hpp"
 #include "delivery_box.hpp"
@@ -9,7 +9,7 @@
 class Vehicle
 {
     private:
-        //Vision vision;
+        Vision vision;
         Movement movement;
         RadioCommunication communication;
         DeliveryBox box;
