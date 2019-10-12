5f4d7b8d9cd97acdb75251d8ed7fecdeb9147ce3
Matheus Dias <matheusgiovannidias@live.com>
2019-10-12 15:14:50 -0300

    Updated App

--- app/godot/dlvr/scripts/PasswField.gd
+++ app/godot/dlvr/scripts/PasswField.gd
@@ -6,13 +6,15 @@ const EMPTY_COLOR := Color(0.804, 0.804, 0.804)
 const VALID_COLOR := Color(0.518, 0.714, 0.898)
 const INVALID_COLOR := Color(0.898, 0.714, 0.518)
 
+var equals_to:PasswField = null
 
 var valid := false
 export(int) var min_size = -1
+export(NodePath) var passw_equals:NodePath = "" setget set_passw_equals
 
 
 func _ready():
-	pass
+	set_passw_equals(passw_equals)
 
 
 func set_valid():
@@ -30,11 +32,19 @@ func set_empty():
 	valid = false
 
 
+func set_passw_equals(val):
+	passw_equals = val
+	equals_to = (get_node(val) as PasswField)
+
+
 func _draw():
 	var value = self.text
+	var valid_map = true
+	valid_map = valid_map and (min_size <= value.length())
+	valid_map = valid_map and ((equals_to == null) or (equals_to.text == value))
 	if value == "":
 		set_empty()
-	elif min_size <= value.length():
+	elif valid_map:
 		set_valid()
 	else:
 		set_invalid()
