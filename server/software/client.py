f4b8fc13fb3b902213a419c66cf387e07c907d29
natanjunges <natanajunges@gmail.com>
2019-10-16 09:10:14 -0300

    Updated client

--- server/software/client.py
+++ server/software/client.py
@@ -2,15 +2,17 @@ import urllib.request
 import ssl
 import json
 
-def getFromURL(url, data_dict, method = "POST"):
+def getFromURL(url, data_dict, method = "POST", cookie = None):
     data = json.dumps(data_dict).encode("utf-8")
     headers = {"Content-Type": "application/json"}
+    if cookie:
+        headers["Cookie"] = cookie
     req = urllib.request.Request(url, data, headers, method = method)
     resp = urllib.request.urlopen(req, context = ssl._create_unverified_context())
     return resp
 
 def main():
-    data = {"email": "natanajunges@gmail.com", "password": "jn1ua9ntg9ae8ns"}
+    data = {"email": "subject@example.com", "password": "Password123!"}
     resp = getFromURL("https://ec2-18-229-140-84.sa-east-1.compute.amazonaws.com/user/signin", data)
     print(resp.read().decode("utf-8"))
     print(resp.getheader("Set-Cookie"))
