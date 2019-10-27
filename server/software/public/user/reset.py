cf7dedfab7f4ce9e81357321005d95c418cf62d3
natanjunges <natanajunges@gmail.com>
2019-10-27 20:29:18 -0300

    Migrated basic user scripts to websockets

--- server/software/public/user/reset.py
+++ server/software/public/user/reset.py
@@ -3,40 +3,48 @@ import connect
 import mysql.connector
 import os
 import hashlib
-import client
+import asyncio
+import websockets
+import ssl
 
-def main(handler):
-    if handler.command == "POST":
-        if "Content-Type" in handler.headers and handler.headers["Content-Type"] == "application/json":
-            data = json.loads(handler.rfile.read(int(handler.headers["Content-Length"])).decode("utf-8"))
-            if "email" in data and "code" in data and "password" in data:
-                handler.send_response(200)
-                handler.end_headers()
-                resp = client.ServerConnection("https://ec2-18-229-140-84.sa-east-1.compute.amazonaws.com").send("/code/verify", {"email": data["email"], "number": data["code"]})
-                if resp.text == "true":
-                    connection = connect.connect()
-                    cursor = connection.cursor(prepared = True)
-                    query = "UPDATE User SET salt = %s, hash = %s WHERE email = %s"
-                    salt = os.urandom(32)
-                    hash = hashlib.pbkdf2_hmac("sha256", data["password"].encode("utf-8"), salt, 100000)
-                    values = (salt.hex(), hash.hex(), data["email"],)
-                    try:
-                        cursor.execute(query, values)
-                        connection.commit()
-                        handler.wfile.write("true".encode("utf-8"))
-                    except mysql.connector.Error:
-                        connection.rollback()
-                        handler.wfile.write("false".encode("utf-8"))
-                    cursor.close()
-                    connection.close()
-                else:
-                    handler.wfile.write("false".encode("utf-8"))
+async def main(websocket, path):
+    data = await websocket.recv()
+    data = json.loads(data)
+    if "email" in data and "code" in data and "password" in data:
+        resp = {
+            "status_code": 200,
+            "reason_message": "OK"
+        }
+        CERT_PEM_PATH = '../cert.pem'
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        ssl_context.load_verify_locations(CERT_PEM_PATH)
+        uri = "wss://ec2-18-229-140-84.sa-east-1.compute.amazonaws.com"
+        async with websockets.connect(uri + "/code/verify", ssl = ssl_context) as websocket2:
+            data2 = {"email": data["email"], "number": data["code"]}
+            await websocket2.send(json.dumps(data2))
+            resp2 = await websocket2.recv()
+            resp2 = json.loads(resp2)
+            if resp2["message_body"] == "true":
+                connection = connect.connect()
+                cursor = connection.cursor(prepared = True)
+                query = "UPDATE User SET salt = %s, hash = %s WHERE email = %s"
+                salt = os.urandom(32)
+                hash = hashlib.pbkdf2_hmac("sha256", data["password"].encode("utf-8"), salt, 100000)
+                values = (salt.hex(), hash.hex(), data["email"],)
+                try:
+                    cursor.execute(query, values)
+                    connection.commit()
+                    resp["message_body"] = "true"
+                    await websocket.send(json.dumps(resp))
+                except mysql.connector.Error as e:
+                    print("reset.py:40: " + str(e))
+                    connection.rollback()
+                    resp["message_body"] = "false"
+                    await websocket.send(json.dumps(resp))
+                cursor.close()
+                connection.close()
             else:
-                handler.send_error(400)
-                handler.end_headers()
-        else:
-            handler.send_error(400)
-            handler.end_headers()
+                resp["message_body"] = "false"
+                await websocket.send(json.dumps(resp))
     else:
-        handler.send_error(501)
-        handler.end_headers()
+        await websocket.send("{\"status_code\": 400, \"reason_message\": \"Bad Request\"}")
---
+++ server/software/websocketclient.py
@@ -0,0 +1,73 @@
+import asyncio
+import websockets
+import ssl
+import json
+
+CERT_PEM_PATH = './cert.pem'
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.load_verify_locations(CERT_PEM_PATH)
+
+def main():
+    asyncio.get_event_loop().run_until_complete(handler())
+
+async def handler():
+    uri = "wss://ec2-18-229-140-84.sa-east-1.compute.amazonaws.com"
+    data = {"email": "subject@example.com", "password": "Password123!", "first_name": "Subject", "last_name": "Subject"}
+    async with websockets.connect(uri + "/user/signup", ssl = ssl_context) as websocket:
+        await websocket.send(json.dumps(data))
+        resp = await websocket.recv()
+        resp = json.loads(resp)
+        if resp["status_code"] == 200 and resp["message_body"] == "true":
+            print("Signup efetuado com sucesso")
+            code = int(input())
+            data = {"email": "subject@example.com", "number": code}
+            async with websockets.connect(uri + "/code/verify", ssl = ssl_context) as websocket2:
+                await websocket2.send(json.dumps(data))
+                resp = await websocket2.recv()
+                resp = json.loads(resp)
+                if resp["status_code"] == 200 and resp["message_body"] == "true":
+                    print("Código verificado com sucesso")
+                    data = {"email": "subject@example.com"}
+                    async with websockets.connect(uri + "/code/generate", ssl = ssl_context) as websocket3:
+                        await websocket3.send(json.dumps(data))
+                        code = int(input())
+                        data = {"email": "subject@example.com", "code": code, "password": "Password456!"}
+                        async with websockets.connect(uri + "/user/reset", ssl = ssl_context) as websocket4:
+                            await websocket4.send(json.dumps(data))
+                            resp = await websocket4.recv()
+                            resp = json.loads(resp)
+                            if resp["status_code"] == 200 and resp["message_body"] == "true":
+                                print("Senha redefinida com sucesso")
+                                data = {"email": "subject@example.com", "password": "Password456!"}
+                                async with websockets.connect(uri + "/user/signin", ssl = ssl_context) as websocket5:
+                                    await websocket5.send(json.dumps(data))
+                                    resp = await websocket5.recv()
+                                    resp = json.loads(resp)
+                                    if resp["status_code"] == 200 and resp["message_body"] == "true":
+                                        print("Signin efetuado com sucesso")
+                                        print("Cookie: {}".format(resp["set_cookie"]))
+                                        data = {"cookie": resp["set_cookie"], "password": "Password456!"}
+                                        async with websockets.connect(uri + "/user/delete", ssl = ssl_context) as websocket6:
+                                            await websocket6.send(json.dumps(data))
+                                            resp = await websocket6.recv()
+                                            resp = json.loads(resp)
+                                            if resp["status_code"] == 200 and resp["message_body"] == "true":
+                                                print("Conta deletada com sucesso")
+                                            else:
+                                                print(resp)
+                                                print("Erro em deletar a conta")
+                                    else:
+                                        print(resp)
+                                        print("Signin incorreto")
+                            else:
+                                print(resp)
+                                print("Erro em redefinir senha")
+                else:
+                    print(resp)
+                    print("Código incorreto")
+        else:
+            print(resp)
+            print("Signup incorreto")
+
+if __name__ == "__main__":
+    main()
