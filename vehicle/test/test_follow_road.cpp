9d86ec54dd6f2d797bbe6be95203ee66500365a7
Álefe Felipe Gonçalves Pereira Dias <alefe@alunos.utfpr.edu.br>
2019-11-10 12:26:27 -0300

    improve follow road

--- vehicle/test/test_follow_road.cpp
+++ vehicle/test/test_follow_road.cpp
@@ -25,26 +25,39 @@ void tick(void* args) {
 }
 
 
-void goAhead(std::vector<street_lines::StreetSection>& found_streets, Movement& movement, float precision)
+void goAhead(std::vector<street_lines::StreetSection>& found_streets, Movement& movement, float precision, float distance)
 {
     street_lines::StreetSection closed_street;
-    float min_angle = 500;
+    float min_angle = 30;
     std::cout << "linha encontradas " << std::endl;
     bool found = false;
-    for(auto sec : found_streets)
+    std::vector<cv::Vec2f> lines;
+    for(auto& sec : found_streets)
     {
-        float angle = setAngleInRange(sec.line[1], (20*M_PI)/180);
-        std::cout << sec.end_points << " angulo: " << angle <<std::endl;
-        if(abs(angle) < min_angle)
+        lines.push_back(sec.line);
+    }
+    auto result = groupLinesByAngle(lines, (precision*M_PI)/180);
+    for(auto& group : result)
+    {
+        float angle = setAngleInRange(lines[group[0]].line[1], (20*M_PI)/180);
+        if((abs(angle) < min_angle) && (!found))
         {
-            found = true;
+            for(auto &sec : group)
+            {
+                float distance = street_lines::distXYPointXYSegment(cv::Point(0,0), found_streets[sec].end_points;
+                if(distance < min_dist)
+                {
+                    found = true;
+                    min_distance = distance;
+                    closed_street = found_streets[sec];
+                }
+            }
             if(angle >= 0)
                 min_angle = angle;
             else if((angle + 180) < abs(angle))
                 min_angle = (angle + 180);
             else
                 min_angle = angle;
-            closed_street = sec;
         }
     }
     if(!found)
@@ -56,16 +69,19 @@ void goAhead(std::vector<street_lines::StreetSection>& found_streets, Movement& 
     {
         movement.goStraight(0, 0);
         if(min_angle >= 0)
-            movement.turn(-precision/2);
+            movement.turn(-precision/4);
         else
-            movement.turn(precision/2);
+            movement.turn(precision/4);
         //movement.goCurve(1, found_streets[0].direction);
+        //return 1;
     }
     else
     {
         std::cout << "andando reto" <<std::endl;
-        movement.goStraight(1, 300);
+        movement.goStraightCm(1, distance, 300);
+        //return 2;
     }
+    //return 0;
 }
 
 int main ()
@@ -90,7 +106,7 @@ int main ()
         }
         if(!found_streets.empty())
         {
-            goAhead(found_streets, movement, 10);
+            while(goAhead(found_streets, movement, 10) != ;
         }
         else
         {
