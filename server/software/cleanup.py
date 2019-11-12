e7d89072fc340bb5597d077ef532c21e4188c6a0
natanjunges <natanajunges@gmail.com>
2019-11-12 16:19:40 -0300

    Added cleanup server script

---
+++ server/software/cleanup.py
@@ -0,0 +1,17 @@
+#!/#!/usr/bin/env python3
+
+import objects
+
+robot = objects.Robot(0)
+robot.state = 0
+robot.position = 0
+for delivery in robot.deliveries:
+    if delivery.state < 5:
+        delivery.delete()
+robot.alive = False
+user1 = objects.User("subject@example.com")
+for login in user1.logins:
+    login.delete()
+user2 = objects.User("subject2@example.com")
+for login in user2.logins:
+    login.delete()
