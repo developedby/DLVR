f9bc2db1bccdc3a01e5f7e78dfbfc4a7bf676a08
natanjunges <natanajunges@gmail.com>
2019-11-12 21:41:38 -0300

    Added debug commands in server

--- server/software/websocketclient.py
+++ server/software/websocketclient.py
@@ -16,6 +16,11 @@ def main():
 
 async def handler():
     uri = "wss://ec2-18-229-140-84.sa-east-1.compute.amazonaws.com"
+    print("Options:")
+    print("0 - Signs in and signs out")
+    print("1 - Signs up and signs out")
+    print("2 - Signs up and deletes account")
+    print("Enter one option: ", end = "")
     option = int(await loop.run_in_executor(None, input))
     if option > 0:
         data = {"email": "subject@example.com", "first_name": "Subject", "last_name": "Subject", "password": "Password123!"}
@@ -24,7 +29,8 @@ async def handler():
             resp = await websocket.recv()
             resp = json.loads(resp)
             if resp["status_code"] == 200 and resp["message_body"] == "true":
-                print("Signup efetuado com sucesso")
+                print("Signup done successfully")
+                print("Enter the verification code: ", end = "")
                 number = int(await loop.run_in_executor(None, input))
                 data = {"user": "subject@example.com", "number": number}
                 async with websockets.connect(uri + "/code/verify", ssl = ssl_context) as websocket2:
@@ -32,20 +38,20 @@ async def handler():
                     resp = await websocket2.recv()
                     resp = json.loads(resp)
                     if resp["status_code"] == 200 and resp["message_body"] == "true":
-                        print("Código verificado com sucesso")
+                        print("Code verified successfully")
                     else:
                         print(resp)
-                        print("Código incorreto")
+                        print("Incorrect code")
             else:
                 print(resp)
-                print("Signup incorreto")
+                print("Incorrect signup")
     data = {"email": "subject@example.com", "password": "Password123!"}
     async with websockets.connect(uri + "/user/signin", ssl = ssl_context) as websocket:
         await websocket.send(json.dumps(data))
         resp = await websocket.recv()
         resp = json.loads(resp)
         if resp["status_code"] == 200 and resp["message_body"] == "true":
-            print("Signin efetuado com sucesso")
+            print("Signin done successfully")
             cookie = resp["set_cookie"]
             print("Cookie: {}".format(cookie))
             DEST = 28
@@ -56,13 +62,15 @@ async def handler():
                 if "path" in resp:
                     if resp["path"] == "/delivery/request":
                         id = resp["message_body"]["id"]
-                        print("Nova solicitação de delivery")
+                        print("New delivery request")
+                        print("Press enter to continue...", end = "")
                         await loop.run_in_executor(None, input)
                         data = {"path": "/delivery/response", "cookie": cookie, "id": id, "accept": True, "destination": DEST}
                         await websocket.send(json.dumps(data))
                     elif resp["path"] == "/robot/update":
                         if "qr" in resp["message_body"]:
-                            print("Robô abriu")
+                            print("Robot opened")
+                            print("Press enter to continue...", end = "")
                             await loop.run_in_executor(None, input)
                             data = {"path": "/delivery/finish", "cookie": cookie, "id": id}
                             await websocket.send(json.dumps(data))
@@ -71,16 +79,18 @@ async def handler():
                             else:
                                 data = {"path": "/user/signout", "cookie": cookie}
                             await websocket.send(json.dumps(data))
+                            print("Press enter to continue...", end = "")
                             await loop.run_in_executor(None, input)
                             return
                         elif "position" in resp["message_body"] and resp["message_body"]["position"] == DEST:
-                            print("Robô chegou ao destino")
+                            print("Robot arrived at destination")
+                            print("Press enter to continue...", end = "")
                             await loop.run_in_executor(None, input)
                             data = {"path": "/delivery/qr", "cookie": cookie, "id": id}
                             await websocket.send(json.dumps(data))
         else:
             print(resp)
-            print("Signin incorreto")
+            print("Incorrect signin")
 
 if __name__ == "__main__":
     main()
