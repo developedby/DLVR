fa05d20a86b769a26f2e1dc53c7e9cc3cb224d12
Matheus Dias <matheusgiovannidias@live.com>
2019-11-27 14:04:58 -0300

    Added Traffic Lights Controller Software

---
+++ city/traffic_controller/traffic_controller.ino
@@ -0,0 +1,28 @@
+
+/**
+ *  DLVR Project
+ *  City Traffic Lights Controller
+ *  @author Matheus Dias (@BulbThinker)
+ */
+
+const int light0 = 1;
+const int light1 = 2;
+const int base_time = 30000;
+
+void setup() {
+    // Lights 0
+    pinMode(light0, OUTPUT);    //Set mode to output
+    digitalWrite(light0, HIGH); //Output to high, leds off
+    // Lights 1
+    pinMode(light1, OUTPUT);    //Set mode to output
+    digitalWrite(light1, HIGH); //Output to high, leds off
+}
+
+void loop() {
+    digitalWrite(light0, LOW);  //Lights 0 On 
+    digitalWrite(light1, HIGH); //Lights 1 Off
+    delay(base_time);           //Delay base time
+    digitalWrite(light0, HIGH); //Lights 0 Off 
+    digitalWrite(light1, LOW);  //Lights 1 On
+    delay(base_time);           //Delay base time
+}
