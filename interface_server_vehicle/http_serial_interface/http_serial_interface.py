c239e13444092e28a399b20953cc24c5e9668a3a
Álefe Felipe Gonçalves Pereira Dias <alefe@alunos.utfpr.edu.br>
2019-11-28 21:13:07 -0300

    made interface of path

--- interface_server_vehicle/http_serial_interface/http_serial_interface.py
+++ interface_server_vehicle/http_serial_interface/http_serial_interface.py
@@ -51,101 +51,12 @@ class SerialInterface:
                 received_packet = []
         return received_packet
 
-    def test_echo(self):
-        #server
-        #variables from server
-        route = [1, 1, 2, 2, 3, 3, 4, 4]
-        qr_code_destination = 0x0005
-        required_status = "status_robot"
-        device_id = 1
-        sensor_to_read = "ultrasound"
-        command = "open_box"
-        #variables to server
-        qr_codes_read = bytearray()
-        sensor_reading = 0
-        status = ""
-        #dictionaries to server
-        movement = {"amplitude" : 0.00, "curvature" : 0.00}
-
-        #vehicle
-        #dictionaries to vehicle
-        vehicle_sensors_code = {"item detector" : 1, "ultrasound" : 2, "lock" : 3}
-        possible_commands = {"force_go_ahead" : 1, "open_box" : 2, "close_box" : 3, "get qr_code" : 4}
-        possible_status_to_vehicle = {"movement" : 1, "status_robot" : 2}
-        #dictionaries from vehicle
-        possible_status_from_vehicle = {1 : "stopped", 2 : "moving forward", 3 :"moving backway", 4 : "avoiding obstacle", 5 : "waiting semaphore", 6 : "waiting route"}
-        #packet serial
-        packet_to_vehicle = []
-        packet_from_vehicle = []
-        radio_address = "DLVR1"
-
-        #build packet to vehicle
-        packet_to_vehicle.append(START_BYTE)
-        packet_to_vehicle.extend(list(radio_address.encode("ascii")))
-        packet_to_vehicle.append(device_id)
-        packet_content = (1 if route else 0) << 3
-        packet_content += (1 if command else 0) << 2
-        packet_content += (1 if sensor_to_read else 0) << 1
-        packet_content += (1 if required_status else 0) << 0
-        packet_to_vehicle.append(packet_content)
-        if route:
-            packet_to_vehicle.append(len(route))
-            packet_to_vehicle.extend(route)
-            packet_to_vehicle.append((qr_code_destination & 0xff00) >> 8)
-            packet_to_vehicle.append((qr_code_destination & 0xff))
-        if command:
-            packet_to_vehicle.append(possible_commands[command])
-        if sensor_to_read:
-            packet_to_vehicle.append(vehicle_sensors_code[sensor_to_read])
-        if required_status:
-            packet_to_vehicle.append(possible_status_to_vehicle[required_status])
-
-        packet_to_vehicle.append(START_BYTE)
-        print(packet_to_vehicle)
-        packet_to_vehicle = bytes(packet_to_vehicle)
-        print(type(packet_to_vehicle), len(packet_to_vehicle), packet_to_vehicle)
-        self.ser.write(packet_to_vehicle)
-
-        byte = self.ser.read()
-        start_byte_byte = START_BYTE.to_bytes(1, byteorder='big')
-        if byte == start_byte_byte:
-            print("legal")
-            packet_from_vehicle = self.ser.read_until(start_byte_byte, 50)
-        #packet_from_vehicle = self.ser.readall()
-        print(type(packet_from_vehicle), len(packet_from_vehicle), packet_from_vehicle)
-
-
-        #interpret packet from vehicle
-
-        if (packet_from_vehicle and packet_from_vehicle[-1] == START_BYTE
-                and packet_from_vehicle[0] == device_id):
-            current_address = 1
-            if sensor_to_read == "ultrasound":
-                sensor_read = struct.unpack('f', packet_from_vehicle[current_address:(current_address+4)])
-                current_address += 4
-            else:
-                    sensor_read = packet_from_vehicle[current_address]
-                    current_address += 1
-            if required_status == "movement":
-                movement["amplitude"] = struct.unpack('f', packet_from_vehicle[current_address:(current_address+4)])
-                current_address += 4
-                movement["curvature"] = struct.unpack('f',packet_from_vehicle[current_address:(current_address+4)])
-                current_address += 4
-                status = movement
-            elif required_status == "status_robot":
-                status = possible_status_from_vehicle[packet_from_vehicle[current_address]]
-                current_address += 1
-            qr_codes_read += packet_from_vehicle[current_address : -1]
-            print("sensor: ", sensor_read)
-            print("status: ", status)
-            print(list(qr_codes_read))
-        print("fim")
-
 class VehicleServerInterface:
     #dictionaries to vehicle
     vehicle_sensors_code = {"item detector" : 1, "ultrasound" : 2, "lock" : 3}
     possible_commands = {"force_go_ahead" : 1, "open_box" : 2, "close_box" : 3, "get qr_code" : 4}
     possible_status_to_vehicle = {"movement" : 1, "status_robot" : 2}
+    directions_to_vehicle = {"back" : 2, "right": 6, "left": 4, "front" : 8}
     #dictionaries from vehicle
     possible_status_from_vehicle = {1 : "stopped", 2 : "moving forward", 3 :"moving backway", 4 : "avoiding obstacle", 5 : "waiting semaphore", 6 : "waiting route"}
     
@@ -175,8 +86,7 @@ class VehicleServerInterface:
         required_status = ""
         qr_code_destination = 0
         if("route" in msg):
-            route = msg["route"]
-            qr_code_destination = msg["qr"]
+            route, qr_code_destination = self.decodePathFromServer(msg["route"])
         if("command" in msg):
             command = msg["command"]
             if(command == "get qr_code"):
@@ -204,6 +114,7 @@ class VehicleServerInterface:
             packet_to_vehicle.append(self.vehicle_sensors_code[sensor_to_read])
         if required_status:
             packet_to_vehicle.append(self.possible_status_to_vehicle[required_status])
+        print(packet_to_vehicle)
         return packet_to_vehicle, sensor_to_read, required_status
 
     def decodeMessageFromVehicle(self, msg, sensor_to_read, required_status, json):
@@ -239,9 +150,20 @@ class VehicleServerInterface:
         dict_from_vehicle["qr"] = qr_codes_read
         return dict_from_vehicle
 
+    def decodePathFromServer(self, path):
+        decoded_path = []
+        qr_code_destination = 0
+        for i in range(0, len(path) - 1):
+            decoded_path.append(self.directions_to_vehicle[path[i][0]])
+            decoded_path.append(int(path[i][1] * 100))
+        qr_code_destination = path[len(path) - 1][2]
+        return decoded_path, qr_code_destination
+
+
 vehicle_server = VehicleServerInterface()
 
 def updateServer():
+    print("requisitando status")
     msg = {"required_status" : "status_robot"}
     data = vehicle_server.handle_server_request(1, msg)
     return data
@@ -272,12 +194,10 @@ async def handler():
                 if "path" in resp:
                     if resp["path"] == "/delivery/response": #tem o caminho ate o inicio da entrega
                         print("Iniciando delivery")
-                        msg = []
+                        msg = {}
                         if("path" in resp["message_body"]):
                             route = resp["message_body"]["path"]
-                            qr_code_destination = resp["message_body"]["qr"]
                             msg["route"] = route
-                            msg["qr"] = qr_code_destination
                             print("tem uma rota")
                         msg["command"] : "close_box"
                         msg["required_status"] = "status_robot"
@@ -292,12 +212,10 @@ async def handler():
                         print("Container aberto")
                     elif resp["path"] == "/delivery/send":#tem o caminho para o local de entrega
                         print("Container fechado")
-                        msg = []
+                        msg = {}
                         if("path" in resp["message_body"]):
                             route = resp["message_body"]["path"]
-                            qr_code_destination = resp["message_body"]["qr"]
                             msg["route"] = route
-                            msg["qr"] = qr_code_destination
                             print("tem uma rota")
                         msg["command"] : "close_box"
                         msg["required_status"] = "status_robot"
@@ -313,6 +231,7 @@ async def handler():
                     elif resp["path"] == "/delivery/qr": #tem o qr code para ver se o valor esta correto
                         print("QR code read")
                         msg = {"command" : "get qr_code"}
+                        msg["qr"] = resp["message_body"]
                         msg["required_status"] = "status_robot"
                         data = vehicle_server.handle_server_request(1, msg)
                         data["signature"] = private_key.sign(hashlib.sha256(json.dumps(data, sort_keys = True).encode("utf-8")).hexdigest().encode("utf-8"), '')[0]
@@ -320,12 +239,10 @@ async def handler():
 
                     elif resp["path"] == "/delivery/finish": #tem o caminho para a garagem
                         print("Delivery finished")
-                        msg = []
+                        msg = {}
                         if("path" in resp["message_body"]):
                             route = resp["message_body"]["path"]
-                            qr_code_destination = resp["message_body"]["qr"]
                             msg["route"] = route
-                            msg["qr"] = qr_code_destination
                             print("tem uma rota")
                         msg["command"] : "close_box"
                         msg["required_status"] = "status_robot"
